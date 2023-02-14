917e1d0b8c6a6bb0dee5d5c4628ea05a90c55b41
ttkapostol <ttkapostolaki@gmail.com>
2023-02-14 09:30:22 +0100

    Add notes

--- js/main.js
+++ js/main.js
@@ -10,5 +10,27 @@ const attempts = document.querySelector('.js__attempts')
 
 //funciones
 
+//1.randomnumber
+
+function getRandomNumber(max) {
+return Math.ceil(Math.random() * max);
+}
+
+const number = getRandomNumber(100);
+console.log(number);
+
+/*2.Validar el número de la jugadora
+----- El número debe estar
+entre 1 y 100. */
+
+/*3.Comparar el número que la jugadora ha escrito en el input con el número aleatorio y pintar las pistas correspondientes en la pantalla.
+----- demasiado alto
+----- demasiado bajo
+----- Has ganado campeona!!!
+*/
+
+//4. Contar los intentos y pintarlos en js__attempts 
+
 
 //eventos
+btn.addEventListener ('click', handleClickBtn);
