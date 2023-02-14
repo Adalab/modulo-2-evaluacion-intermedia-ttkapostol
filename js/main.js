939133cf93e576d437bd3d14fc96d7b4c948dbac
ttkapostol <ttkapostolaki@gmail.com>
2023-02-14 10:55:37 +0100

    Add validate function

--- js/main.js
+++ js/main.js
@@ -6,33 +6,26 @@ console.log(`linked`);
 const userNumber = document.querySelector('.js__user_number')
 const btn = document.querySelector('.js__btn')
 const tips = document.querySelector('.js__tips')
-const attempts = document.querySelector('.js__attempts')
+const userAttempts = document.querySelector('.js__attempts')
 
 //funciones
-
-//1.randomnumber
 
 function getRandomNumber(max) {
 return Math.ceil(Math.random() * max);
 }
 
-
-
-/*2.Validar el número de la jugadora
------ El número debe estar
-entre 1 y 100. */
-
-/*3.Comparar el número que la jugadora ha escrito en el input con el número aleatorio y pintar las pistas correspondientes en la pantalla.
-*/
 const showMsj = (msj) => {
   tips.innerHTML = msj;
 };
 
+const showMsj2 = (msj) => {
+  userAttempts.innerHTML = msj;
+};
+
 function compare() {
-  const number = getRandomNumber(100);
+const number = getRandomNumber(100);
   console.log(number);
-  
-  const getNumber = userNumber.value
+const getNumber = parseInt(userNumber.value)
   if (getNumber === number) {
     showMsj(`Has ganado campeona!!! :)`);
   } else if (getNumber < number - 10) {
@@ -42,41 +35,25 @@ function compare() {
   }
 }
 
-/*
------ `Número demasiado alto :( `
------ `Número demasiado bajo :( `
------ `Has ganado campeona!!! :)`*/
-
-/*function compare() {
-  const number = getRandomNumber(100);
-  console.log(number);
-  const userNumber = userNumber.value
-  
-
-  //if(number  ) {
-  if (number === 0) {
-    mainElement.innerHTML = 'Te ha tocao la lotería';
-  } else if (isEven(number)) {
-    mainElement.classList.remove('chileanFireOrange');
-    mainElement.classList.add('correctYellow');
+function validateIput() {
+const getNumber = parseInt(userNumber.value)
+  if (userNumber.value === '') {
+    showMsj('¡Tienes que poner un número si quieres jugar!');
+  } else if (getNumber < 0 || getNumber > 100) {
+    showMsj('¡Tienes que poner un número entre 0 y 100!');
   } else {
-    mainElement.classList.remove('correctYellow');
-    mainElement.classList.add('chileanFireOrange');
+    compare()
   }
 }
-*/
 
-//4. Contar los intentos y pintarlos en js__attempts 
+//3. Contar los intentos y pintarlos en js__attempts 
 
-//Don't forget el prevent default
 
 //eventos
 
 function handleClickBtn(event) {
   event.preventDefault();
-  console.log(`btn works!`);
-  //funciones
-  compare();
+  validateIput();
 }
 
 btn.addEventListener ('click', handleClickBtn);
