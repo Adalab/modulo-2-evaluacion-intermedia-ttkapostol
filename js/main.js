e28e5832f782ef2fb2ffc1282a3c89cdd2b2bb91
ttkapostol <ttkapostolaki@gmail.com>
2023-02-14 09:13:12 +0100

    Add main variables

---
+++ js/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+console.log(`linked`);
+
+//variables
+const userNumber = document.querySelector('.js__user_number')
+const btn = document.querySelector('.js__btn')
+const tips = document.querySelector('.js__tips')
+const attempts = document.querySelector('.js__attempts')
+
+//funciones
+
+
+//eventos
